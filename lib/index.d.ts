94f1b451faa43cb08713c3ab7d95f5810d3ffc55
Chandler-Davidson <cdavidson@3-gis.com>
2020-01-14 09:05:43 -0600

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,8 @@
 import { NowRequest, NowResponse } from "@now/node";
-export declare type RequestHandler = (request: NowRequest, response: NowResponse) => [NowResponse, any];
+export declare type RequestHandler = (request: NowRequest, response: NowResponse) => [NowResponse, any] | Promise<[NowResponse, any]>;
 export declare type HttpMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
 export default class NowRouter {
     private map;
     constructor(mappings: [HttpMethod, RequestHandler][]);
-    handle(request: NowRequest, response: NowResponse): [NowResponse, any];
+    handle(request: NowRequest, response: NowResponse): Promise<[NowResponse, any]>;
 }
